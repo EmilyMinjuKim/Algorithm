3397c01d5c30c55764c662d037225d480500ca99
mingblee <mingblee@172.30.1.15>
2020-07-06 23:15:54 +0900

    gg

--- src/level1/GcdLcm.java
+++ src/level1/GcdLcm.java
@@ -1,11 +1,35 @@
 package level1;
 
-public class GcdLcm {
-	
-}
+import java.util.Arrays;
 
-class Main{
-	public static void main(String args[]) {
+class Main {
+
+	public static void main(String[] args) {
+		
+		GcdLcm sol = new GcdLcm();
+		System.out.println(Arrays.toString(sol.solution(3, 12)));
 		
 	}
+}
+
+class GcdLcm {
+	  public int[] solution(int n, int m) {
+	      int[] answer = new int[2];
+	      int lcm = n*m;
+	      
+	      answer[0] = gcd(n, m);
+	      answer[1] = lcm / answer[0];
+	   
+	      return answer;
+	      
+	}
+	 
+	  
+	  public int gcd(int a, int b) {
+		  if(b == 0) {
+			  return a;
+		  } else {
+			  return gcd(b, a%b);
+		  }
+	  }
 }
