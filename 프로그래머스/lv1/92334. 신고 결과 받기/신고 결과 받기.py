9daba2d1ca59a5712be19727eee913d2a9ed6f71
EmilyMinjuKim <65878320+EmilyMinjuKim@users.noreply.github.com>
2023-04-11 08:28:27 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,25 +1,12 @@
 def solution(id_list, report, k):
-    rp_list = [[] for _ in range(len(id_list))]
-    answer, rp_cnt = [0] * len(id_list), [0] * len(id_list)
-    final_list = []
+    answer = [0] * len(id_list)    
+    reports = {x : 0 for x in id_list}
 
-    for i in report:
-        each_list = i.split()
-        s = id_list.index(each_list[0])
-        e = id_list.index(each_list[1])
-        if e not in rp_list[s]:
-            rp_cnt[e] += 1
-            rp_list[s].append(e)
-            
-    for i in range(len(rp_cnt)):
-        if rp_cnt[i] >= k:
-            final_list.append(i)
+    for r in set(report):
+        reports[r.split()[1]] += 1
 
-    for idx in range(len(rp_list)):
-        for i in rp_list[idx]:
-            if i in final_list:
-                answer[idx] += 1
-    
-    
-    
+    for r in set(report):
+        if reports[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
+
     return answer
