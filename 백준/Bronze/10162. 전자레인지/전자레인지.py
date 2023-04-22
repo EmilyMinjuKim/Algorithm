97bc27c68403dd1bdbc6196396460679a1724181
EmilyMinjuKim <65878320+EmilyMinjuKim@users.noreply.github.com>
2023-04-22 17:16:36 +0900

    [Bronze III] Title: 전자레인지, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10162. 전자레인지/전자레인지.py
+++ 백준/Bronze/10162. 전자레인지/전자레인지.py
@@ -1,15 +1,6 @@
-n = int(input())
+t = int(input())
 
-# A B C : 300 60 10
-button = [300,60,10]
-cnt = [0] * 3
-
-for i in range(3):
-    if n >= button[i]:
-        cnt[i] = n // button[i]
-        n %= button[i]
-
-if n != 0:
+if t%10 != 0:
     print(-1)
 else:
-    print(*cnt)
+    print(t//300,(t%300)//60,(t%60)//10)
